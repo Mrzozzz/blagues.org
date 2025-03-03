6dee9ab5ce058ea9a7fbc67bfc60ef01707c2617
Enzo COME <enzo.come@univ-rouen.fr>
2025-03-03 09:31:03 +0100

    Modification des boutons

--- code.js
+++ code.js
@@ -6,3 +6,20 @@ function toggleAnswer(id) {
         answer.style.display = "none";
     }
 }
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("btn1").addEventListener("click", function () {
+        toggleAnswer("answer1");
+    });
+    document.getElementById("btn2").addEventListener("click", function () {
+        toggleAnswer("answer2");
+    });
+    document.getElementById("btn3").addEventListener("click", function () {
+        toggleAnswer("answer3");
+    });
+    document.getElementById("btn4").addEventListener("click", function () {
+        toggleAnswer("answer4");
+    });
+    document.getElementById("btn5").addEventListener("click", function () {
+        toggleAnswer("answer5");
+    });
+})
